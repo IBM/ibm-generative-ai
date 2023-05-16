b6d0f9b62cc778b44461a664dfb75518c3f8e709
vdemers <vdemers@ibm.com>
2023-05-16 10:41:42 -0400

    Testing furo as html theme

--- documentation/docs/source/conf.py
+++ documentation/docs/source/conf.py
@@ -32,5 +32,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "furo"
+# html_theme = "alabaster"
 html_static_path = ["_static"]
