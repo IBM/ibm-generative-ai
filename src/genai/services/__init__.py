44e6f182247751f7166fd1103fbff304e71bee1c
vdemers <vdemers@ibm.com>
2023-06-08 15:58:24 -0400

    Replaced imports

--- src/genai/services/__init__.py
+++ src/genai/services/__init__.py
@@ -1,6 +1,7 @@
+#noqa
 from genai.services.async_generator import AsyncResponseGenerator
-from genai.services.prompt_service_interface import PromptServiceInterface
 from genai.services.request_handler import RequestHandler
 from genai.services.service_interface import ServiceInterface
+from genai.services.prompt_service_interface import PromptServiceInterface
 
 __all__ = ["RequestHandler", "ServiceInterface", "AsyncResponseGenerator", "PromptServiceInterface"]
