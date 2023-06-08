5e1140e94cff9165eca1de8159fb6c133ba93b51
James Sutton <1068763+jpwsutton@users.noreply.github.com>
2023-06-08 16:28:24 +0200

    Adding Localserver Extension (#9)
    * feat: initial cut of localserver extension
    * fix: fixing localserver test to only import when marked
    * fix: improving example import documentation of transformers lib
    * fix: moving RequestBody schemas into specific file in localserver extension
    * fix: adding torch and sentencepiece into localserver example instructions
    * feat: moving localserver example to it's own directory with requirements file
    
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>
    Signed-off-by: Mairead O'Neill <moneill@ibm.com>

---
+++ src/genai/extensions/localserver/__init__.py
@@ -0,0 +1,4 @@
+from genai.extensions.localserver.custom_model_interface import CustomModel
+from genai.extensions.localserver.local_api_server import LocalLLMServer
+
+__all__ = ["CustomModel", "LocalLLMServer"]
