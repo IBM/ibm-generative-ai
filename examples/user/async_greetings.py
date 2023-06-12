0de5294833711409e2a9743a8883d4bf63232928
Adriana Meza Soria <Adriana.Meza.Soria@ibm.com>
2023-06-12 14:06:56 -0400

    Revert "open-source presentation examples"
    This reverts commit beb2012d64cb8ba9fa13ca900696871f93a7dc23.
    
    Signed-off-by: Adriana Meza Soria <Adriana.Meza.Soria@ibm.com>

--- examples/user/async_greetings.py
+++ examples/user/async_greetings.py
@@ -1,13 +1,10 @@
 import os
-import time
 
 from dotenv import load_dotenv
 
 from genai.credentials import Credentials
 from genai.model import Model
 from genai.schemas import GenerateParams, ModelType
-
-start_time = time.time()
 
 # make sure you have a .env file under genai root with
 # GENAI_KEY=<your-genai-key>
@@ -32,18 +29,13 @@ creds = Credentials(api_key, api_endpoint)
 model = Model(ModelType.FLAN_UL2, params=params, credentials=creds)
 
 greeting = "Hello! How are you?"
-lots_of_greetings = [greeting] * 100
+lots_of_greetings = [greeting] * 1000
 num_of_greetings = len(lots_of_greetings)
 num_said_greetings = 0
 greeting1 = "Hello! How are you?"
 
 # yields batch of results that are produced asynchronously and in parallel
-for result in model.generate_async(lots_of_greetings, hide_progressbar=True):
+for result in model.generate_async(lots_of_greetings):
     num_said_greetings += 1
     print(f"[Progress {str(float(num_said_greetings/num_of_greetings)*100)}%]")
-    if result is not None:
-        print(f"\t {result.generated_text}")
-    else:
-        print("\t <the result was 'None' for this input>")
-
-print("--- %s seconds ---" % (time.time() - start_time))
+    print(f"\t {result.generated_text}")
