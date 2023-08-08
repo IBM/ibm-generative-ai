3a962d72704764a43d2131c44491abee3c09877e
Mairead O'Neill <moneill@ibm.com>
2023-08-08 10:04:09 -0400

    feat: improved messaging on terms of use error
    Signed-off-by: Mairead O'Neill <moneill@ibm.com>

--- src/genai/exceptions/genai_exception.py
+++ src/genai/exceptions/genai_exception.py
@@ -15,11 +15,15 @@ class GenAiException(Exception):
             try:
                 self.error = ErrorResponse(**error.json())
                 self.error_message = self.error.message
+                
             except ValidationError:
                 self.error = error
                 self.error_message = str(error.content)
         else:
             self.error = error
             self.error_message = str(error)
+        if "TOU_NOT_ACCEPTED" in self.error_message:
+            split_message = self.error_message.split("Terms of use not accepted")
+            self.error_message = "".join([split_message[0], "Terms of use not accepted. Please accept the terms of use in a browser.", split_message[1]])
         logger.error(self.error_message)
         super().__init__(self.error_message)
