a90c2ceb51250c6f1ee10c1a81f3ab6d36f9fb96
Tomáš Dvořák <toomas2d@gmail.com>
2023-11-13 21:31:48 +0100

    fix(tests): disable ssl certificate (#233)

--- examples/user/prompts_adult_dataset/prompt_generation_with_column_idx.py
+++ examples/user/prompts_adult_dataset/prompt_generation_with_column_idx.py
@@ -25,7 +25,7 @@ def get_dataset(datafile):
     if os.path.exists(datafile):
         print("Data already downloaded. Skipping the download.")
         return
-    with requests.get(url) as response:
+    with requests.get(url, verify=False) as response:
         print("Saving data to {}".format(datafile))
         # Add data
         with open(datafile, "ab") as f:
