92fc4debdca940156b5f6191e49870136b52fa5c
David Kristek <david@David--MacBook-Pro.local>
2024-01-22 19:45:12 +0100

    feat: final agent example

--- examples/extensions/langchain/langchain_agent.py
+++ examples/extensions/langchain/langchain_agent.py
@@ -1,8 +1,17 @@
+"""LangChain agent using tools to answer questions."""
+
+from typing import Optional
+
 from dotenv import load_dotenv
-from langchain.agents import AgentExecutor, create_structured_chat_agent
+from langchain.agents import AgentExecutor
+from langchain.agents.format_scratchpad import format_log_to_str
+from langchain.agents.output_parsers import JSONAgentOutputParser
+from langchain.tools.render import render_text_description_and_args
+from langchain_core.callbacks import CallbackManagerForToolRun
 from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.tools import tool
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.tools import BaseTool
 
 from genai import Client, Credentials
 from genai.extensions.langchain import LangChainChatInterface
@@ -11,13 +20,23 @@ from genai.text.generation import TextGenerationParameters
 load_dotenv()
 
 
-@tool
-def get_word_length(word: str) -> int:
-    """Returns the length of a word."""
-    return len(word)
+class WordLengthTool(BaseTool):
+    name = "GetWordLength"
+    description = "Returns the length of a word."
+
+    def _run(self, word: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
+        return len(word)
 
 
-tools = [get_word_length]
+tools: list[BaseTool] = [WordLengthTool()]
+
+# @tool
+# def get_word_length(word: str) -> int:
+#     """Returns the length of a word."""
+#     return len(word)
+
+
+# tools = [get_word_length]
 
 system = """Respond to the human as helpfully and accurately as possible. You have access to the following tools:
 {tools}
@@ -46,7 +65,7 @@ Action:
   "action": "Final Answer",
   "action_input": "Final response to human"
 }}
-Begin! Reminder to ALWAYS respond with a valid json blob of a single action. Use tools if necessary.
+Begin! Reminder to ALWAYS respond with a valid json blob of a single action.
 Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation"""
 human = """{input}
 {agent_scratchpad}
@@ -59,15 +78,29 @@ prompt = ChatPromptTemplate.from_messages(
         ("human", human),
     ]
 )
+
 client = Client(credentials=Credentials.from_env())
 llm = LangChainChatInterface(
     client=client,
     model_id="meta-llama/llama-2-70b-chat",
     parameters=TextGenerationParameters(max_new_tokens=250, min_new_tokens=20, temperature=0),
 )
+prompt = prompt.partial(
+    # get tools with their descriptions and args in plain text
+    tools=render_text_description_and_args(list(tools)),
+    tool_names=", ".join([t.name for t in tools]),
+)
+llm_with_stop = llm.bind(stop=["Observation"])
 
-
-agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=prompt)
+agent = (
+    RunnablePassthrough.assign(
+        # format the agent's scratchpad to a string
+        agent_scratchpad=lambda x: format_log_to_str(x["intermediate_steps"]),
+    )
+    | prompt
+    | llm_with_stop
+    | JSONAgentOutputParser()
+)
 agent_executor = AgentExecutor(agent=agent, tools=tools, handle_parsing_errors=True, verbose=True)
 chat_history = []
 
