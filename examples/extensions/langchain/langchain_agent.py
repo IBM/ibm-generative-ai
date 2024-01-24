5ce371fd399f2bf22d4c2ab23f88468e933ef97f
David Kristek <david@David--MacBook-Pro.local>
2024-01-24 15:12:50 +0100

    fixup! Merge branch 'main' into feat-agent-example

--- examples/extensions/langchain/langchain_agent.py
+++ examples/extensions/langchain/langchain_agent.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 from langchain.agents import AgentExecutor
 from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.agents.output_parsers import JSONAgentOutputParser
+from langchain.memory import ConversationBufferMemory
 from langchain.tools.render import render_text_description_and_args
 from langchain_core.callbacks import CallbackManagerForToolRun
-from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.tools import BaseTool
@@ -24,13 +24,13 @@ class WordLengthTool(BaseTool):
     name = "GetWordLength"
     description = "Returns the length of a word."
 
-    def _run(self, word: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> str:
+    def _run(self, word: str, run_manager: Optional[CallbackManagerForToolRun] = None) -> int:
         return len(word)
 
 
 tools: list[BaseTool] = [WordLengthTool()]
 
-system = """Respond to the human as helpfully and accurately as possible. You have access to the following tools:
+system_prompt = """Respond to the human as helpfully and accurately as possible. You have access to the following tools:
 {tools}
 Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input).
 Valid "action" values: "Final Answer" or {tool_names}
@@ -59,15 +59,15 @@ Action:
 }}
 Begin! Reminder to ALWAYS respond with a valid json blob of a single action.
 Respond directly if appropriate. Format is Action:```$JSON_BLOB```then Observation"""
-human = """{input}
+human_prompt = """{input}
 {agent_scratchpad}
 (reminder to respond in a JSON blob no matter what)"""
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", system),
+        ("system", system_prompt),
         MessagesPlaceholder("chat_history", optional=True),
-        ("human", human),
+        ("human", human_prompt),
     ]
 )
 
@@ -75,29 +75,30 @@ client = Client(credentials=Credentials.from_env())
 llm = LangChainChatInterface(
     client=client,
     model_id="meta-llama/llama-2-70b-chat",
-    parameters=TextGenerationParameters(max_new_tokens=250, min_new_tokens=20, temperature=0),
+    parameters=TextGenerationParameters(
+        max_new_tokens=250, min_new_tokens=20, temperature=0, stop_sequences=["\nObservation"]
+    ),
 )
 prompt = prompt.partial(
     # get tools with their descriptions and args in plain text
     tools=render_text_description_and_args(list(tools)),
     tool_names=", ".join([t.name for t in tools]),
 )
-llm_with_stop = llm.bind(stop=["Observation"])
+
+memory = ConversationBufferMemory()
 
 agent = (
     RunnablePassthrough.assign(
         # format the agent's scratchpad to a string
         agent_scratchpad=lambda x: format_log_to_str(x["intermediate_steps"]),
+        # pass the memory as the chat history
+        chat_history=lambda x: memory.chat_memory.messages,
     )
     | prompt
-    | llm_with_stop
+    | llm
     | JSONAgentOutputParser()
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, handle_parsing_errors=True, verbose=True)
-chat_history = []
+agent_executor = AgentExecutor(agent=agent, tools=tools, handle_parsing_errors=True, verbose=True, memory=memory)
 
-input1 = "How many letters are in the word educa?"
-result = agent_executor.invoke({"input": input1, "chat_history": chat_history})
-
-chat_history.extend([HumanMessage(content=input1), AIMessage(content=result["output"])])
-agent_executor.invoke({"input": "That's not a real word, can you tell me a valid word?", "chat_history": chat_history})
+agent_executor.invoke({"input": "How many letters are in the word educa?"})
+agent_executor.invoke({"input": "That's not a real word, can you tell me a valid word?"})
