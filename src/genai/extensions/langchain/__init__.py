b4e4d4ea94e3ddc2875eabdac6f0363a956d22c8
Tomáš Dvořák <toomas2d@gmail.com>
2024-01-26 22:38:07 +0100

    feat(langchain): add embeddings support (#289)
    Ref: #288

--- src/genai/extensions/langchain/__init__.py
+++ src/genai/extensions/langchain/__init__.py
@@ -1,6 +1,7 @@
 """Extension for LangChain library"""
 
 from genai.extensions.langchain.chat_llm import LangChainChatInterface
+from genai.extensions.langchain.embeddings import LangChainEmbeddingsInterface
 from genai.extensions.langchain.llm import LangChainInterface
 from genai.extensions.langchain.template import (
     from_langchain_template,
@@ -10,6 +11,7 @@ from genai.extensions.langchain.template import (
 __all__ = [
     "LangChainInterface",
     "LangChainChatInterface",
+    "LangChainEmbeddingsInterface",
     "from_langchain_template",
     "to_langchain_template",
 ]
