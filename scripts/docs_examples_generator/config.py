9a7dc080d408f2cf2892ccbd268b406f3bbc89b5
Tomas Dvorak <toomas2d@gmail.com>
2024-02-02 18:38:53 +0100

    ci(docs): fix env branch name
    Signed-off-by: Tomas Dvorak <toomas2d@gmail.com>

--- scripts/docs_examples_generator/config.py
+++ scripts/docs_examples_generator/config.py
@@ -7,14 +7,14 @@ from pydantic import BaseModel, Field
 dirname = Path(__file__).parent.absolute()
 dotenv.load_dotenv()
 
+_branch_name = os.environ["BRANCH_NAME"]
+
 
 class GeneratorConfig(BaseModel):
     output_directory: Path = Path(dirname, "../../documentation/source/rst_source")
     input_directory: Path = Path(dirname, "../../examples")
     module_name: str = Field("examples", min_length=1)
-    input_directory_href: str = (
-        f"https://github.com/IBM/ibm-generative-ai/blob/{os.environ.get('BRANCH_NAME', 'main')}/examples"
-    )
+    input_directory_href: str = f"https://github.com/IBM/ibm-generative-ai/blob/{_branch_name}/examples"
     exclude: set[str] = set()
     modules_order: dict[str, dict[str, list[int]]] = {
         "examples": [
