db9f880038bbb96154869d072075443684e134c1
Tomáš Dvořák <toomas2d@gmail.com>
2024-02-02 16:53:48 +0100

    feat(langchain): validate peer  dependency (#308)

--- examples/extensions/langchain/__init__.py
+++ examples/extensions/langchain/__init__.py
@@ -6,4 +6,12 @@ LangChain
 
     To use the following extension, first install it by running
     :bash:`pip install 'ibm-generative-ai[langchain]'`.
+
+
+.. admonition:: LangChain versions
+    :class: attention
+
+    Our LangChain extension depends on `langchain-core` library.
+    In case you are also using `langchain` library, please do ensure you are using version >= 0.1.0
+    to prevent possible bugs.
 """
