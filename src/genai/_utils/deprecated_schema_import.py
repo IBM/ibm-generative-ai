a450890479a22de68746caa9663ea9792477e3d7
Tomas Dvorak <toomas2d@gmail.com>
2024-02-24 22:42:49 +0100

    feat: add fallback for deprecated/removed schemas

--- src/genai/_utils/deprecated_schema_import.py
+++ src/genai/_utils/deprecated_schema_import.py
@@ -3,13 +3,12 @@ import warnings
 _CACHED_WARNINGS: set[str] = set()
 
 
-def _log_deprecation_warning(name: str, module_name: str):
+def _log_deprecation_warning(name: str, module_name: str, msg: str):
     cache_warning_name = f"{module_name}.{name}"
 
     if cache_warning_name in _CACHED_WARNINGS:
         return
 
-    msg = f"Deprecated import of {name} from module {module_name}. Please use `from genai.schema import {name}`."
     with warnings.catch_warnings():
         warnings.simplefilter("always", DeprecationWarning)
         warnings.warn(msg, category=DeprecationWarning, stacklevel=4)  # the original import is 4 levels higher
@@ -25,7 +24,11 @@ def _deprecated_schema_import(name: str, module_name: str):
     import genai.schema
 
     if name in dir(genai.schema) and not name.startswith("_"):
-        _log_deprecation_warning(name, module_name)
+        _log_deprecation_warning(
+            name,
+            module_name,
+            f"Deprecated import of {name} from module {module_name}. Please use `from genai.schema import {name}`.",
+        )
         return getattr(genai.schema, name)
 
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
--- src/genai/schema/__init__.py
+++ src/genai/schema/__init__.py
@@ -1,3 +1,42 @@
 from genai.schema._api import *
+from genai.schema._api_removed_schemas import _removed_schemas
 from genai.schema._endpoints import *
 from genai.schema._extensions import *
+
+_removed_schema_aliases = {
+    "UserPromptResult": PromptResult,
+    "PromptsResponseResult": PromptResult,
+    "UserResponseResult": UserResult,
+    "UserCreateResultApiKey": UserApiKey,
+    "PromptRetrieveRequestParamsSource": PromptListSource,
+}
+
+
+def __getattr__(name):
+    if name in globals():
+        return globals()[name]
+
+    if name in _removed_schemas:
+        from genai._utils.deprecated_schema_import import _log_deprecation_warning
+
+        explanation, target = _removed_schemas[name]
+        _log_deprecation_warning(
+            name=name,
+            module_name=__name__,
+            msg=explanation,
+        )
+        return target
+
+    if name in _removed_schema_aliases:
+        from genai._utils.deprecated_schema_import import _log_deprecation_warning
+
+        target = _removed_schema_aliases[name]
+        _log_deprecation_warning(
+            name=name,
+            module_name=__name__,
+            msg=f"Class has been renamed from `{name}` to `{target.__name__}`."
+            f"This class alias will be removed in the following major release.",
+        )
+        return target
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
