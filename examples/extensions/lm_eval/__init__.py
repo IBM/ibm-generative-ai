9a97422e707d000f6ab07426b421408f7e43f4e1
Radek Ježek <pc.jezek@gmail.com>
2024-04-12 09:29:15 +0200

    feat: add lm_eval extension (#345)
    Signed-off-by: Radek Ježek <pc.jezek@gmail.com>

---
+++ examples/extensions/lm_eval/__init__.py
@@ -0,0 +1,9 @@
+"""
+lm_eval
+
+.. admonition:: Before you start
+    :class: important
+
+    To use the following extension, first install it by running
+    :bash:`pip install 'ibm-generative-ai[lm_eval]'`.
+"""
